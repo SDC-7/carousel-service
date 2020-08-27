1bfaff6ce1bf7647160086412129c0b6904fd057
Meredith Myers <meredith@myers1.com>
2020-08-26 19:03:14 -0500

    Implemented CRUD API

--- server/index.js
+++ server/index.js
@@ -2,12 +2,21 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-
 app.get(`/api/images/:location`, (req, res) => {
+  /*
+  EXISTING IMPLEMENTATION
+  db.query('SELECT * FROM images WHERE location_id = ? ORDER BY img_order ASC', [id], (err, data) => {
+    if (err) {
+      res.send('An error occurred');
+    } else {
+      res.send(data);
+    }
+  });*/
   const location = req.params.location;
   res.send(`GET request images for location #${location}`)
 })
 
+// START NOT APPLICABLE CRUD API REQUESTS
 app.post(`/api/images/:location`, function (req, res) {
   const location = req.params.location;
   res.send(`POST request images for location #${location}`)
@@ -22,6 +31,7 @@ app.delete(`/api/images/:location`, function (req, res) {
   const location = req.params.location;
   res.send(`DELETE request images for location #${location}`)
 })
+// END NOT APPLICABLE CRUD API REQUESTS
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
